207a5d1be79dc239eb4221873cd8213e57610930
Mahmood Farhat Ali <sence79.s7.s9@gmail.com>
2020-07-23 23:24:45 +0200

    finished store

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,6 +1,35 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
+
+    public static boolean pass_check(char pass[]){
+boolean alphabits=false,nums=false;
+    if(pass.length>6){
+        for(int i=0;i<pass.length;i++){
+            if((pass[i]>='a' && pass[i]<='z')  || (pass[i]>='A' && pass[i]<='Z'))
+            {
+                alphabits=true;
+            }
+            else if(pass[i]>='0' && pass[i]<='9'){
+                nums=true;
+            }
+
+            if(alphabits==true && nums==true){
+                return true;
+            }
+        }
+
+    }
+
+        return false;
+
+
+}
+
+
+
+
 
     public static void main(String[] args) {
 Shop shopA=new Shop();
@@ -8,6 +37,7 @@ Shop shopA=new Shop();
         while(sign==1) {
             System.out.println("for Customer->>>press 1 \nfor Store->>>press2");
             int choice = scan.nextInt();
+
             if (choice == 1) {
                 System.out.println("press 1 for customer login\npress 2 for customer registeration");
                 choice = scan.nextInt();
@@ -17,11 +47,17 @@ Shop shopA=new Shop();
                     System.out.println("please enter your password");
                     String password = scan.next();
                     shopA.login_forcustomer(mobile, password);
-                    if (shopA.login_forcustomer(mobile, password) == -1) {
-                        System.out.println("wrong pass or try registering ");
+                    if (shopA.find_customer(mobile) != -1) {
+                        if(shopA.login_forcustomer(mobile,password)==-1){
+                            System.out.println("wrong pass");
+                        }
+                        else{
+                            System.out.println("OK the customer exists");
+                        }
+
                     }
                     else{
-                        System.out.println("OK the customer exists");
+                        System.out.println("try to register");
                     }
 
                 }
@@ -31,24 +67,77 @@ Shop shopA=new Shop();
                     String customer_mobile = scan.next();
                     System.out.println("please enter your name ");
                     String customer_name = scan.next();
-                    System.out.println("please enter your password ");
-                    String customer_pass = scan.next();
-                    System.out.println("please enter your block number ");
-                    int customer_blocknum = scan.nextInt();
-                    Customer customer = new Customer(customer_mobile, customer_name, customer_pass, customer_blocknum);
-                    if(shopA.add_customer(customer)!=true){
-                        System.out.println("the mobile number already exists try to log in ");
-                    }
-                    else{
-                        shopA.add_customer(customer);
-                        System.out.println("customer has been added ");
-                    }
-
+                   inner:
+                   {
+                       System.out.println("please enter your password ");
+                       String customer_pass = scan.next();
+                       if (pass_check(customer_pass.toCharArray())==false) {
+                           System.out.println("please enter a pass with length>6 and has alphabits....try again");
+                          break inner;
+                       }
+                       System.out.println("please enter your block number ");
+                       int customer_blocknum = scan.nextInt();
+                       Customer customer = new Customer(customer_mobile, customer_name, customer_pass, customer_blocknum);
+                       if (shopA.add_customer(customer) != true) {
+                           System.out.println("the mobile number already exists try to login ");
+                       } else {
+                           shopA.add_customer(customer);
+                           System.out.println("customer has been added ");
+                       }
+                   }
                 }
-                System.out.println("to continue press 1 otherwise press 0 ");sign=scan.nextInt();
-
             }
 
+
+            else if(choice==2){
+                System.out.println("press 1 for store login\npress 2 for store registeration");
+                choice = scan.nextInt();
+                if (choice == 1) {
+                    System.out.println("please enter your username");
+                    String username = scan.next();
+                    System.out.println("please enter your password");
+                    String password = scan.next();
+                    shopA.login_forstore(username, password);
+
+                    if (shopA.find_store(username)!=-1)
+                    {  if(shopA.login_forstore(username,password)==-1){
+                        System.out.println("wrong password ");}
+                        else{
+                        System.out.println("OK the store exists");
+                           }
+
+                    }
+                    else if(shopA.find_store(username)==-1){
+                        System.out.println("store doesn't exist try to register ");
+
+                    }}
+
+                else if(choice==2){
+                        System.out.println("please enter your username ");
+                        String store_username = scan.next();
+                        inner:{
+                            System.out.println("please enter your password ");
+                        String store_pass = scan.next();
+                        if(pass_check(store_pass.toCharArray())!=true){
+                            System.out.println("please enter a pass with length>6 and has alphabits....try again");
+                            break inner;
+                        }
+                        Store store=new Store(new Vector<>(),new Vector<>(),store_username,store_pass);
+                        if(shopA.add_store(store)==false){
+
+                            System.out.println("the store username already exists try to login ");
+                        }
+                        else {
+
+                            shopA.add_store(store);
+                            System.out.println("store has been added ");
+                        }
+                    }
+                }
+
+
+            }
+            System.out.println("to continue press 1 otherwise press 0 ");sign=scan.nextInt();
 
         }
 
--- Project/src/Shop.java
+++ Project/src/Shop.java
@@ -5,7 +5,7 @@ public class Shop {
 
 private Vector<Customer>customers;private Vector<Store>stores;
 
-private int find_customer(String customer_mobile){
+public int find_customer(String customer_mobile){
 
     for(int i=0;i<customers.size();i++){
         if(customers.elementAt(i).getCustomer_mobile().equals(customer_mobile)) {
@@ -33,6 +33,35 @@ public int login_forcustomer(String customer_mobile,String password){
         }
     }
         return -1;
+
+}
+
+public int find_store(String username){
+    for(int i=0;i<stores.size();i++) {
+        if (stores.elementAt(i).getUsername().equals(username)) {
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+public boolean add_store(Store store){
+if(find_store(store.getUsername())!=-1){
+    return false;
+}
+stores.add(store);
+return true;
+}
+
+public int login_forstore(String username,String password){
+if(find_store(username)!=-1){
+    if(stores.elementAt(find_store(username)).getPassword().equals(password)){
+        return find_store(username);
+    }
+}
+return -1;
+
 
 }
 
